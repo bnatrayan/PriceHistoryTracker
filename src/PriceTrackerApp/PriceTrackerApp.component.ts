d4e75a0190c479ed7d4f9338bd855850ccd8878b
bnatrayan <bala.natrayan@gmail.com>
2021-09-28 23:00:40 -0500

    Renamed application name

--- src/PriceTrackerApp/PriceTrackerApp.component.ts
+++ src/PriceTrackerApp/PriceTrackerApp.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './PriceTrackerApp.component.html',
+  styleUrls: ['./PriceTrackerApp.component.css']
 })
 export class AppComponent {
   title = 'PriceHistoryTracker';
